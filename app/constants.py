ffd508f79c2af3c6e473b26fed743fc98ba759f4
chinar <chinar.vartak@konsultera.in>
2026-06-16 14:42:21 +0530

    feat: add PARTNER to UserType and RoleType enums

--- app/constants.py
+++ app/constants.py
@@ -4,8 +4,10 @@ from enum import Enum
 class UserType(str, Enum):
     SUPERADMIN = "SUPERADMIN"
     CUSTOMER = "CUSTOMER"
+    PARTNER = "PARTNER"
 
 
 class RoleType(str, Enum):
     ADMIN = "ADMIN"
     CUSTOMER = "CUSTOMER"
+    PARTNER = "PARTNER"
